8365ce09d7cb12ebddd54b73e1739c6a20aa70d8
tdronfield <tay.dronfield@gmail.com>
2023-02-02 19:03:38 -0500

    Revamp to coffee theme

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,10 @@
-import ChatMessage from "./components/TheMessageComponent.js";
-import Nickname from "./components/TheNicknameComponent.js";
+import ChatMessage from './components/TheMessageComponent.js';
+import Nickname from './components/TheNicknameComponent.js';
 
 (() => {
-    console.log('fired');
+    function loadingScreen(){
+        console.log('Launching!');
+    }
 
     const socket = io();
 
@@ -24,45 +26,36 @@ import Nickname from "./components/TheNicknameComponent.js";
         vm.username=nickname;
     }
 
+    loadingScreen();
 
     const vm = new Vue({
         data: {
             messages: [],
-            nickname: "",
-            socketID: "",
-            message: "",
-            username: "",
+            nickname: '',
+            socketID: '',
+            message: '',
+            username: '',
             showUserChange: false,
-            
-        },
-
-        created: function() {
-            console.log("it's alive!!");
+            isTyping: ''
         },
 
         methods: {
             dispatchMessage(){
                 // debugger;
-                socket.emit("chatmessage", {content: this.message, name: this.nickname || "Anonymous" });
+                socket.emit('chatmessage', {content: this.message, name: this.nickname || 'Anonymous' });
             
-                this.message = "";
+                this.message = '';
             },
 
             changeNickname(){
-                // socket.emit("nickname", {name: this.nickname});
-                // debugger;
                 this.nickname = event.target.previousElementSibling.value;
             },
 
             toggleChangeUsername(){
-                console.log("Opening");
-
                 event.target.nextElementSibling.classList.toggle('show-change-user');
             },
 
             hideChangeUsername(){
-                console.log("Closing");
-                // debugger;
                 event.target.parentElement.classList.remove('show-change-user');
             },
         },
@@ -72,9 +65,10 @@ import Nickname from "./components/TheNicknameComponent.js";
             newNickname: Nickname,
         }
 
-    }).$mount("#app");
+    }).$mount('#app');
 
-    socket.addEventListener("connected", setUserId);
-    socket.addEventListener("message", appendMessage);
-    socket.addEventListener("nickname", appendNickname);
+    socket.addEventListener('connected', setUserId);
+    socket.addEventListener('message', appendNickname);
+    socket.addEventListener('message', appendMessage);
+    socket.addEventListener('nickname', appendNickname);
 })();
